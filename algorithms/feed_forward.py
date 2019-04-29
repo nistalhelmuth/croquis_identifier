249946b7fbbbb88b19c87df3dac5285992ad5499
Unknown <nis16104@uvg.edu.gt>
2019-04-29 16:06:42 -0600

    despues  optimizar feed_forward

--- algorithms/feed_forward.py
+++ algorithms/feed_forward.py
@@ -3,15 +3,41 @@ import numpy as np
 def sigmoidfunc(x):
     return 1.0/(1 + np.exp(-x))
 
-def newsigmoid(x):
-    return 1/(1 + np.exp(-10*x+5))
+def tanh(x):
+    return 2*sigmoidfunc(2*x) - 1 
+
+def softmax(x):
+    e_x = np.exp(x - x.max())
+    return e_x / e_x.sum()
   
 def othersigmoid(x):
-    return 1/(1 + np.exp(-0.01*x))
+    x = softmax(x-784)
+    return sigmoidfunc(x)
+    #return 1/(1 + np.exp(-0.01*x))
 
-def feed_forward_bias(X, thetas): #tres capas (una oculta)
+def sigmoid_transformation(theta,A):
+    z = np.matmul(theta,A)
+    delta = z.max() - z.min()
+    x = ((z - z.min() - delta*0.5) * 6)/delta
+    return sigmoidfunc(x)
+
+def softmax_transformation(theta,A):
+    z = np.matmul(theta,A)
+    print(z)
+    delta = z.max() - z.min()
+    x = ((z - z.min() - delta*0.5) * 6)/delta
+    print(x.min())
+    print(x.max())
+    return softmax(x)
+
+def feed_forward_bias(X, thetas, show = False): #tres capas (una oculta)
     A1 = np.vstack((1,X))
-    A2 = np.vstack((1,sigmoidfunc(np.matmul(thetas[0],A1))))
-    A3 = np.vstack((1,sigmoidfunc(np.matmul(thetas[1],A2))))
-    A4 = sigmoidfunc(np.matmul(thetas[2],A3))
+    A2 = np.vstack((1,sigmoid_transformation(thetas[0],A1)))
+    A3 = np.vstack((1,sigmoid_transformation(thetas[1],A2)))
+    #A4 = softmax_transformation(thetas[2],A3)
+    #A2 = np.vstack((1,othersigmoid( np.matmul(thetas[0],A1) )))
+    #A3 = np.vstack((1,othersigmoid( np.matmul(thetas[1],A2) )))
+    A4 = softmax(np.matmul(thetas[2],A3))
+    if show:
+        print(A4)
     return (A1, A2, A3, A4)
--- algorithms/train.py
+++ algorithms/train.py
@@ -1,13 +1,14 @@
 import sys
+import os
 import numpy as np
 from PIL import Image
 from feed_forward import feed_forward_bias
 
 def calculate_phi(A, thetas,spected_result):
     phi3 = (spected_result - A[3]) ** 2 * -1
-    phi2 = np.matmul( thetas[2].T, phi3 ) * A[2] * (1-A[2]) 
-    phi1 = np.matmul( thetas[1].T, phi2[1:] ) * A[1] * (1-A[1]) 
-    phi0 = np.matmul( thetas[0].T, phi1[1:] ) * A[0] * (1-A[0])
+    phi2 = np.matmul( thetas[2].T, phi3 ) * (A[2]*100) * (1-A[2]) 
+    phi1 = np.matmul( thetas[1].T, phi2[1:] ) * (A[1]*100) * (1-A[1]) 
+    phi0 = np.matmul( thetas[0].T, phi1[1:] ) * (A[0]*100) * (1-A[0])
     return (phi0, phi1, phi2, phi3)
 
 def update_delta(deltas,phis,A):
@@ -16,7 +17,8 @@ def update_delta(deltas,phis,A):
     delta3 = deltas[0] + np.matmul(phis[1][1:],A[0].T)
     return (delta3, delta2, delta1)
 
-def tobinary(x):
+def binary(x):
+    #return x
     if x:
         return 1
     return 0
@@ -27,65 +29,69 @@ def back_propagation_bias(
     spected_result, 
     image_path
     ):
-    images = os.listdir(path)
+    images = os.listdir(image_path)
+    print("with ",len(images)," samples")
     for i in images:#por cada imagen en una categoria
-        #img = Image.open("../images/huevo/"+str(1+i)+".bmp")
-        img = Image.open(path+i)
-        img = tobinary(np.array(img)[:,:,0].reshape(-1,1))
+        img = Image.open(image_path+"/"+i)
+        if (".bmp" in i):
+            img = tobinary(np.array(img)[:,:,0].reshape(-1,1))
+        else: 
+            img = tobinary(np.array(img).reshape(-1,1))
         A = feed_forward_bias(img, thetas)
         phis = calculate_phi(A, thetas,spected_result)
         deltas = update_delta(deltas, phis, A)
-    return (deltas[0]/len(image), deltas[1]/len(image),deltas[2]/len(image))
+    return (deltas[0]/len(images), deltas[1]/len(images),deltas[2]/len(images))
 
 np.set_printoptions(threshold=sys.maxsize)
-
+tobinary = np.vectorize(binary)
 #pesos
 theta1 = np.random.rand(10,785)
 theta2 = np.random.rand(10,11)
-theta3 = np.random.rand(2,11)
+theta3 = np.random.rand(10,11)
 thetas = (theta1, theta2, theta3)
 
 #cambios
 delta1 = np.zeros(10*785).reshape(10,785)
 delta2 = np.zeros(10*11).reshape(10,11)
-delta3 = np.zeros(2*11).reshape(2,11)
+delta3 = np.zeros(10*11).reshape(10,11)
 deltas = (delta1, delta2, delta3)
 
 #herramientas
-categories = ["Square","SmileyFace"]
-results = np.array([0,0]).reshape(-1,1) 
+categories = os.listdir("../images/")
+results = np.array([0,0,0,0,0,0,0,0,0,0]).reshape(-1,1) 
 
 #before train
 print("Square")
-img = Image.open("../images/cuadrado.bmp")
+img = Image.open("../cuadrado.bmp")
+#print(np.array(img)[:,:,0])
 img = tobinary(np.array(img)[:,:,0].reshape(-1,1))
-A = feed_forward_bias(img, thetas)
-print(A[3])
+feed_forward_bias(img, thetas, show = True)
+
 
 print("SmileyFace")
-img = Image.open("../images/carafeliz.bmp")
+img = Image.open("../carafeliz.bmp")
+#print(np.array(img)[:,:,0])
 img = tobinary(np.array(img)[:,:,0].reshape(-1,1))
-A = feed_forward_bias(img, thetas)
-
-print(A[3])
+feed_forward_bias(img, thetas, show = True)
 
 #train part
 for category in range(0,len(categories)):
     print("training ",categories[category],"...")
-    spected_result = results
+    spected_result = results.copy()
     spected_result[category] += 1
+    
     image_path = "../images/"+categories[category]
     deltas = back_propagation_bias(thetas,deltas, spected_result, image_path)
-    thetas = (thetas[0] + deltas[0], thetas[1] + deltas[1], thetas[2] + deltas[2])
+    thetas = (thetas[0] - deltas[0], thetas[1] - deltas[1], thetas[2] - deltas[2])
 
+print("DONE!!!")
 #after train
 print("Square")
-img = Image.open("../images/cuadrado.bmp")
+img = Image.open("../cuadrado.bmp")
 img = tobinary(np.array(img)[:,:,0].reshape(-1,1))
-A = feed_forward_bias(img, thetas)
-print(A[3])
+feed_forward_bias(img, thetas, show = True)
 
 print("SmileyFace")
-img = Image.open("../images/carafeliz.bmp")
+img = Image.open("../carafeliz.bmp")
 img = tobinary(np.array(img)[:,:,0].reshape(-1,1))
-A = feed_forward_bias(img, thetas)
+feed_forward_bias(img, thetas, show = True)
